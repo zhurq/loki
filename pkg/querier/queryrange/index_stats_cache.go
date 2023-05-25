5a68d9bf65ed332ced14970821e41551e361a4f6
Salva Corts <salva.corts@grafana.com>
2023-05-25 11:55:08 +0200

    Stub now time in test

--- pkg/querier/queryrange/index_stats_cache.go
+++ pkg/querier/queryrange/index_stats_cache.go
@@ -73,6 +73,10 @@ func (cfg *IndexStatsCacheConfig) ShouldCache(req queryrangebase.Request, now mo
 	return cfg.DoNotCacheRequestWithin == 0 || model.Time(req.GetEnd()).Before(now.Add(-cfg.DoNotCacheRequestWithin))
 }
 
+// statsCacheMiddlewareNowTimeFunc is a function that returns the current time.
+// It is used to allow tests to override the current time.
+var statsCacheMiddlewareNowTimeFunc = model.Now
+
 func NewIndexStatsCacheMiddleware(
 	cfg IndexStatsCacheConfig,
 	log log.Logger,
@@ -98,7 +102,8 @@ func NewIndexStatsCacheMiddleware(
 			if shouldCache != nil && !shouldCache(r) {
 				return false
 			}
-			return cfg.ShouldCache(r, model.Now())
+			now := statsCacheMiddlewareNowTimeFunc()
+			return cfg.ShouldCache(r, now)
 		},
 		parallelismForReq,
 		retentionEnabled,
